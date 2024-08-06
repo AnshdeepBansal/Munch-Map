b6dc44a85c674748aa5a0aa1b86556738de174d0
Anshdeep Bansal <udaibansal123@gmail.com>
2024-08-07 01:25:07 +0530

    creating menu inside the res-cards

--- src/RestaurantMenu.jsx
+++ src/RestaurantMenu.jsx
@@ -10,12 +10,14 @@ const RestaurantMenu = ()=>{
         const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=" + resId);
         const json = await data.json();
         console.log(json);
+        const temp = json?.data?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards || json?.data?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards || json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards || json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards;
+        console.log(temp);
     }
     return (
         <>
             <div className="Menu">
                 <div className="Menu-card">
-                    <h1>roll</h1>
+                    <h1>{resId}</h1>
                     <h1>momo</h1>
                     <h1>paneer</h1>
                     <h1>daal</h1>
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -11,9 +11,10 @@ const Body = ()=>{
     const [filteredList , setfilterList] = useState(listofresturant);
     const lat = [30.3102486,12.9716,19.0760,28.7041,18.5204,26.8467];
     const long =[78.02096569999999,77.5946,72.8777,77.1025,73.8567,80.9462];
+    
     const fetch_data = async ()=>{
         console.log("fetching........s");
-        const data = await fetch(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat[5]}&lng=${long[5]}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`);
+        const data = await fetch(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat[0]}&lng=${long[0]}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`);
         const json = await data.json();
         console.log(json);
         const temp = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
@@ -77,7 +78,7 @@ const Body = ()=>{
              </div>
              <div className='res-card-container'>
              {filteredList?filteredList.length == 0?<NotFound/>:filteredList.map((res) => {
-                return <Link key={res?.info?.id} to="/restaurant" ><RestaurantCard  resData={res} /></Link>
+                return <Link key={res?.info?.id} to={"/restaurant/" + res?.info.id} ><RestaurantCard  resData={res} /></Link>
              }):<NotFound/>}
              </div>
         </div>
